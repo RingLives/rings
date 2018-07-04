dbcec9e80905b79c80b83d43fad16fdb82734d63
Shohidur Rahman <sohidurr49@gmail.com>
2018-07-04 14:00:22 +0600

    some change

---
+++ app/Http/Controllers/authenticates/company/TestController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\authenticates\company; //RL24
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+	public function index(){
+		
+	}
+}
